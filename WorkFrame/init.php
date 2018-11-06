83bbaa33598199c710e9ea8283008e5c0abad670
Tom Brown <tom.brown@public-i.info>
2018-11-06 13:11:35 +0000

    workframe no longer intereferes with any ini log settings when WORKFRAME_DEBUG is turned off

--- WorkFrame/init.php
+++ WorkFrame/init.php
@@ -11,8 +11,8 @@ if(WORKFRAME_DEBUG) {
 	error_reporting(E_ALL);
 	ini_set('display_errors','On');
 } else {
-	error_reporting(E_ALL);
-	ini_set('display_errors','Off');
+//	error_reporting(E_ALL);
+//	ini_set('display_errors','Off');
 }
 
 
