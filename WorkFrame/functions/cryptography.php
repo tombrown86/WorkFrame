cb5e1eb15e185976a704683271184365ef91719b
tombrown86 <tombrown86@gmail.com>
2019-05-15 09:58:26 +0100

    Added openssl_encrypt and openssl_decrypt

--- WorkFrame/functions/cryptography.php
+++ WorkFrame/functions/cryptography.php
@@ -15,3 +15,15 @@ function hash_data($secret, $data, $mechanism = 'md5') {
 function check_hash_data($hash, $secret, $data, $mechanism = 'md5') {
 	return $hash === hash_data($secret, $data, $mechanism);
 }
+
+function openssl_encrypt($data, $encryption_key, $alg='aes-256-cbc') {
+	$iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($alg));
+	$encrypted = openssl_encrypt($data, $alg, $encryption_key, 0, $iv);
+	return $encrypted . ':' . $iv;
+}
+
+function openssl_decrypt($encrypted, $encryption_key, $alg='aes-256-cbc') {
+	$parts = explode(':', $encrypted);
+	$decrypted = openssl_decrypt($parts[0], $alg, $encryption_key, 0, $parts[1]);
+	return $decrypted;
+}
