5de5312432493e60bf88c455c8a3d361a9b6ef38
tombrown86 <tombrown86@gmail.com>
2019-05-15 10:23:09 +0100

    Update cryptography.php
    Added encrypt_openssl and decrypt_openssl  helper functions

--- WorkFrame/functions/cryptography.php
+++ WorkFrame/functions/cryptography.php
@@ -15,3 +15,18 @@ function hash_data($secret, $data, $mechanism = 'md5') {
 function check_hash_data($hash, $secret, $data, $mechanism = 'md5') {
 	return $hash === hash_data($secret, $data, $mechanism);
 }
+
+function encrypt_openssl($msg, $key, $alg = 'AES-256-CBC', $openssl_options = 0) {
+	$iv_size = openssl_cipher_iv_length($alg);
+	$iv = openssl_random_pseudo_bytes($iv_size);
+	$encryptedMessage = openssl_encrypt($msg, $alg, $key, $openssl_options, $iv);
+	return base64_encode($iv . $encryptedMessage);
+}
+
+function decrypt_openssl($data, $key, $alg = 'AES-256-CBC', $openssl_options = 0) {
+	$data = base64_decode($data);
+	$iv_size = openssl_cipher_iv_length($alg);
+	$iv = substr($data, 0, $iv_size);
+	$data = substr($data, $iv_size);
+	return openssl_decrypt($data, $alg, $key, $openssl_options, $iv);
+}
