c8fc09e9114c3d4784235da835095e06ec4e9454
tom <tom@HAL-kubuntu>
2019-11-15 18:37:45 +0000

    added jquery 3.4.1 and made it default

--- WorkFrame/config/javascript.php
+++ WorkFrame/config/javascript.php
@@ -4,5 +4,5 @@ return [
 	'use_workframe_functions' => TRUE,
 	'use_workframe_processors' => TRUE,
 	'use_jquery' => TRUE,
-	'jquery_version' => '1.11.3', // or 3.0.0 (the only 2 versions currently bundled with WorkFrame)
+	'jquery_version' => '3.4.1', // Choose 1.11.3, 3.4.1 or 3.0.0 (the 3 versions currently bundled with WorkFrame)
 ];
