51ad10edff4e45f03b7bd266ee820530aeb319cd
tombrown86 <tombrown86@gmail.com>
2022-03-08 09:31:07 +0000

    Update router.php
    Async_processor must also not be instantiated until in try catch

--- WorkFrame/router.php
+++ WorkFrame/router.php
@@ -28,31 +28,30 @@ $routes = array_filter(explode('/', $base_url));
 
 // Handle any Async_processor calls natively
 if(isset($routes[0]) && $routes[0] === '_async_processor') {
-	$processor_class = "\\WorkFrame\\Request_handlers\\Async_processor";
-	$request_handler = new $processor_class();
+	$request_handler_class_path = "\\WorkFrame\\Request_handlers\\Async_processor";
 	$action = 'process';
 	$routes = [];
 } else {
-	$class = "\\" . APP_NAMESPACE . "\\Request_handlers";
+	$class_path = "\\" . APP_NAMESPACE . "\\Request_handlers";
 	// Last segment may or may not be the action. Either case, default to index if < 2 segments
 	$last_segment = (count($routes) > 1) ? array_pop($routes) : 'index';
 
 	foreach ($routes as $route) {
-		$class .= "\\" . ucfirst($route);
+		$class_path .= "\\" . ucfirst($route);
 	}
 
-	if (count($routes) && class_exists($class)) {
+	if (count($routes) && class_exists($class_path)) {
 		$action = $last_segment;
 	} else { // no action specified, last segment part of routes
-		$class .= "\\" . ucfirst($last_segment);
+		$class_path .= "\\" . ucfirst($last_segment);
 		$routes[] = $last_segment;
 		$action = 'index'; // Action not given, default is index
 	}
 
-	if (class_exists($class) && method_exists($class, $action)) {
-		$request_handler_class = $class;
+	if (class_exists($class_path) && method_exists($class_path, $action)) {
+		$request_handler_class_path = $class_path;
 	} else {
-		$request_handler_class = "\\" . APP_NAMESPACE . "\\Request_handlers\\Error\\Error";
+		$request_handler_class_path = "\\" . APP_NAMESPACE . "\\Request_handlers\\Error\\Error";
 		$action = 'error_404';
 		$routes = ['error'];
 		header("HTTP/1.0 404 Not Found");
@@ -63,7 +62,7 @@ handle_route:
 
 // The request may be rewritten at this point (E.g. for 403)
 try {
-	$request_handler = new $request_handler_class();
+	$request_handler = new $request_handler_class_path();
 	// Prepare request handler for execution
 	if(!$request_handler instanceof WorkFrame\Request_handler) {
 		throw new WorkFrame\Exceptions\Class_is_not_a_request_handler_exception;
@@ -77,7 +76,7 @@ try {
 	$request_handler->post_action_hook();
 	$_workframe->post_action_hook();
 } catch (\WorkFrame\Exceptions\Request_handler_rewrite_exception $e) {
-	$request_handler_class = $e->get_rewrite_request_handler_class();
+	$request_handler_class_path = $e->get_rewrite_request_handler_class();
 	$action = $e->get_rewrite_request_handler_action();
 	$routes = $e->get_rewrite_request_routes();
 	goto handle_route;
