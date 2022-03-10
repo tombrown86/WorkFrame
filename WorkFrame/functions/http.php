b5728d92a011c4766a5ddd58ce7f4946594b20ed
tombrown86 <tombrown86@gmail.com>
2022-03-10 12:58:24 +0000

    Added func to set_url_param

--- WorkFrame/functions/http.php
+++ WorkFrame/functions/http.php
@@ -8,3 +8,23 @@ function redirexit($location) {
 	header('Location: '.$location);
 	exit;
 }
+
+/**
+ * Add parameter to URL
+ * @param string $url
+ * @param string $key
+ * @param string $value
+ * @return string result URL
+ */
+function set_url_param($url, $key, $value = null) {
+	$query = parse_url($url, PHP_URL_QUERY);
+	if ($query) {
+		$query_params = null;
+		parse_str($query, $query_params);
+		$query_params[$key] = $value;
+		$url = str_replace("?$query", '?' . http_build_query($query_params), $url);
+	} else {
+		$url .= '?' . urlencode($key) . '=' . urlencode($value);
+	}
+	return $url;
+}
