ca1a6a1da3dd472896f0725f2b3605560bb42d6c
tombrown86 <tombrown86@gmail.com>
2022-05-01 11:58:53 +0100

    Remove duplicated func

--- WorkFrame/js/_workframe_functions.js
+++ WorkFrame/js/_workframe_functions.js
@@ -35,18 +35,3 @@ function wf_get_cookie(cname) {
     }
     return "";
 }
-function wf_get_cookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
