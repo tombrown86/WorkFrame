c22989dfdfa642349915d8a52e50840cdb1c0853
tombrown86 <tombrown86@gmail.com>
2022-05-01 11:55:00 +0100

    Added wf_ucfirst

--- WorkFrame/js/_workframe_functions.js
+++ WorkFrame/js/_workframe_functions.js
@@ -1,6 +1,9 @@
 function wf_h(s) {
 	return $('<div/>').text(s).html().replace(/"/g, '&quot;').replace(/'/g, '&apos;');
 }
+function wf_ucfirst(string) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
 function wf_nl2br(str, is_xhtml) {
     var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
     return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
@@ -32,3 +35,19 @@ function wf_get_cookie(cname) {
     }
     return "";
 } 
+
+function wf_get_cookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
